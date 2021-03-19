3aaa4e44382b8ac06d7ef8af3d2713dcace4a6f1
xdedss <liuzhuoran_2000@163.com>
2021-03-19 21:22:19 +0800

    HOG

---
+++ exp_hog/app.js
@@ -0,0 +1,304 @@
+
+
+
+//(╯‵□′)╯︵┻━┻
+
+$(function(){
+    function reshape(){
+        $('#inputimg').css('height', $('#inputimg').width() + 'px');
+        $('#outputimg').css('height', $('#outputimg').width() + 'px');
+    }
+    
+    // 根据数据类型取合适的指针
+    function correctPtr(mat){
+        switch(mat.type() % 8){
+            case 0: // 8U
+                return mat.ptr.bind(mat);
+                break;
+            case 1: // 8S
+                return mat.charPtr.bind(mat);
+                break;
+            case 2: // 16U
+                return mat.ushortPtr.bind(mat);
+                break;
+            case 3: // 16S
+                return mat.shortPtr.bind(mat);
+                break;
+            case 4: // 32S
+                return mat.intPtr.bind(mat);
+                break;
+            case 5: // 32F
+                return mat.floatPtr.bind(mat);
+                break;
+            case 6: // 64F
+                return mat.doublePtr.bind(mat);
+                break;
+        }
+        return mat.floatPtr.bind(mat);
+    }
+    
+    // 逐像素灰度映射
+    function mapMatGs(mat, mapper){
+        var channels = mat.channels();
+        mapMat(mat, (ptr, res)=>{
+            for (var ci = 0; ci < channels; ci++){
+                res[ci] = mapper(ptr[ci]);
+            }
+        });
+    }
+    
+    // 逐像素映射
+    function mapMat(mat, mapper){
+        var cols = mat.cols;
+        var rows = mat.rows;
+        var ptrGetter = correctPtr(mat);
+        var ptr;
+        var res = [0,0,0,0];
+        var channels = mat.channels();
+        for (var r = 0; r < rows; r++){
+            for (var c = 0; c < cols; c++){
+                ptr = ptrGetter(r, c);
+                mapper(ptr, res);
+                for (var ci = 0; ci < channels; ci++){
+                    ptr[ci] = res[ci];
+                }
+            }
+        }
+    }
+    
+    // 数组除以模
+    function normalizeArr(arr, res){
+        var sqrsum = 0;
+        for (var i = 0; i < arr.length; i++){
+            sqrsum += arr[i] * arr[i];
+        }
+        var mag = Math.sqrt(sqrsum);
+        res = res || [];
+        if (mag == 0){
+            for (var i = 0; i < arr.length; i++){
+                res[i] = 1 / Math.sqrt(arr.length);
+            }
+            return res;
+        }
+        for (var i = 0; i < arr.length; i++){
+            res[i] = arr[i] / mag;
+        }
+        return res;
+    }
+    
+    // 计算一块区域的直方图
+    function localHOG(mag, angle, numBins){
+        var cols = mag.cols;
+        var rows = mag.rows;
+        var magPtrGetter = correctPtr(mag);
+        var anglePtrGetter = correctPtr(angle);
+        var m, a, bin, binfrac;
+        var res = [];
+        for (var i = 0; i < numBins; i++) res[i] = 0;
+        for (var r = 0; r < rows; r++){
+            for (var c = 0; c < cols; c++){
+                m = magPtrGetter(r, c)[0];
+                a = anglePtrGetter(r, c)[0];
+                binfrac = (a % 180) / 180.0 * numBins;
+                bin = Math.floor(binfrac);
+                binfrac -= bin;
+                res[bin] += (1-binfrac) * m;
+                res[(bin >= numBins - 1) ? 0 : (bin+1)] += binfrac * m;
+            }
+        }
+        return res;
+    }
+    
+    // concatenate
+    function cat(arrarr){
+        var res = [];
+        for (var i = 0; i < arrarr.length; i++){
+            for (var j = 0; j < arrarr[i].length; j++){
+                res.push(arrarr[i][j]);
+            }
+        }
+        return res;
+    }
+    
+    function mainProcess(imWidth, imHeight, cellSize, numBins){
+    
+        // 读入图片
+        let mat = cv.imread($('#hidden')[0]);
+        // 调整大小
+        cv.resize(mat, mat, new cv.Size(imHeight, imWidth));
+        
+        // 可视化用画布
+        let visualize = new cv.Mat();
+        mat.copyTo(visualize);
+        // 淡化底图
+        mapMat(visualize, (ptr, res) => {
+            const fade = 3;
+            res[0] = Math.floor(ptr[0] / fade);
+            res[1] = Math.floor(ptr[1] / fade);
+            res[2] = Math.floor(ptr[2] / fade);
+            res[3] = 255;
+        });
+        
+        // 转灰度
+        let gs = new cv.Mat();
+        cv.cvtColor(mat, gs, cv.COLOR_BGR2GRAY);
+        
+        //计算梯度
+        let gx = new cv.Mat();
+        let gy = new cv.Mat();
+        cv.Sobel(gs, gx, cv.CV_32F, 1, 0, 1);
+        cv.Sobel(gs, gy, cv.CV_32F, 0, 1, 1);
+        
+        // 计算模长和角度
+        let mag = new cv.Mat();
+        let angle = new cv.Mat();
+        cv.cartToPolar(gx, gy, mag, angle, 1);
+        
+        // cell数量和窗口滑动步数
+        var cellRows = imHeight / cellSize;
+        var cellCols = imWidth / cellSize;
+        var slideRows = cellRows - 1;
+        var slideCols = cellCols - 1;
+        
+        // 统计每一个cell的直方图
+        var hogDict = {};
+        for (var cr = 0; cr < cellRows; cr++){
+            for (var cc = 0; cc < cellCols; cc++){
+                let windowRect = new cv.Rect(cr * cellSize, cc * cellSize, cellSize, cellSize);
+                let angleWindow = angle.roi(windowRect);
+                let magWindow = mag.roi(windowRect);
+                var hog = localHOG(magWindow, angleWindow, numBins);
+                hogDict[cr+'|'+cc] = hog;
+            }
+        }
+        
+        //按照每一个小cell标准化并可视化
+        var normalBuffer = []
+        for (var i = 0; i < numBins; i++) normalBuffer[i] = 0;
+        for (var cr = 0; cr < cellRows; cr++){
+            for (var cc = 0; cc < cellCols; cc++){
+                var centerX = cc * cellSize + cellSize / 2;
+                var centerY = cr * cellSize + cellSize / 2;
+                var hog = hogDict[cr+'|'+cc];
+                normalizeArr(hog, normalBuffer);
+                for (var anglei = 0; anglei < numBins; anglei++){
+                    var curangle = anglei * Math.PI / numBins;
+                    var length = cellSize * 0.7 * normalBuffer[anglei];
+                    var dx = length * Math.cos(curangle);
+                    var dy = -length * Math.sin(curangle);
+                    cv.line(visualize, new cv.Point(centerY + dy, centerX + dx), new cv.Point(centerY - dy, centerX - dx), new cv.Scalar(255, 255, 255, 255));
+                }
+            }
+        }
+        
+        //按照block标准化连接得到最终结果
+        var res = []
+        for (var i = 0; i < numBins; i++) normalBuffer[i] = 0;
+        for (var cr = 0; cr < slideRows; cr++){
+            for (var cc = 0; cc < slideCols; cc++){
+                var hog = cat([hogDict[cr+'|'+cc], hogDict[(cr+1)+'|'+cc], hogDict[(cr+1)+'|'+cc], hogDict[(cr+1)+'|'+(cc+1)]]);
+                res.push(normalizeArr(hog));
+            }
+        }
+        res = cat(res);
+        var resstr = '';
+        resstr += '结果为' + res.length + '维向量<br>';
+//        for (var i = 0; i < res.length; i++){
+//            resstr += res[i] + ', '
+//        }
+        $('#result').html(resstr);
+        
+        //let res = new cv.Mat();
+        //console.log(mat.size());
+        
+        //process
+        //cv.blur(mat, res, new cv.Size(5, 5));
+        //mapMatGs(mag, c => c /255);
+        //mapMatGs(angle, c => c / 360);
+        
+        cv.imshow('inputimg', mat);
+        cv.imshow('outputimg', visualize);
+        
+        mat.delete();
+        visualize.delete();
+        gs.delete();
+        gx.delete();
+        gy.delete();
+        mag.delete();
+        angle.delete();
+        //res.delete();
+    }
+    
+    $(window).on('resize', reshape);
+    
+    $('#loaded').css('display', 'none');
+    $('#cvjs').on('load', function(){
+        $('#loading').css('display', 'none');
+        $('#loaded').css('display', '');
+        reshape();
+        
+        $('#fileinput').on('change', e=>{
+            if ($('#fileinput')[0].files.length > 0){
+                $('#go').removeClass('disabled');
+            }
+            else{
+                $('#go').addClass('disabled');
+            }
+        });
+        
+        $('#go').on('click', e=>{
+            if ($('#fileinput')[0].files.length > 0){
+                $('#hidden').attr('src', URL.createObjectURL($('#fileinput')[0].files[0]))
+            }
+            else{
+                $('#fileinputgroup').addClass('has-error');
+                $('#fileinputgroup .form-input-hint').html('无效文件');
+            }
+        });
+        
+        $('#hidden').on('load', e=>{
+            var numBins = parseInt($('#parbin').val());
+            var cellSize = parseInt($('#parcell').val());
+            var imWidth = parseInt($('#parwidth').val());
+            var imHeight = parseInt($('#parwidth').val());
+//            console.log(numBins);
+//            console.log(cellSize);
+//            console.log(imHeight);
+            if (!(numBins > 0)){
+                $('#parbingroup').addClass('has-error');
+                $('#parbingroup .form-input-hint').html('无效数字');
+                return;
+            }
+            if (!(cellSize > 0)){
+                $('#parcellgroup').addClass('has-error');
+                $('#parcellgroup .form-input-hint').html('无效数字');
+                return;
+            }
+            if (!(imWidth > 0)){
+                $('#parwidthgroup').addClass('has-error');
+                $('#parwidthgroup .form-input-hint').html('无效数字');
+                return;
+            }
+            
+            if (imWidth % cellSize != 0){
+                $('#parwidthgroup').addClass('has-error');
+                $('#parcellgroup').addClass('has-error');
+                $('#parcellgroup .form-input-hint').html('width必须能被cell整除');
+                return;
+            }
+            
+            $('#fileinputgroup').removeClass('has-error');
+            $('#fileinputgroup .form-input-hint').html('');
+            $('#parbingroup').removeClass('has-error');
+            $('#parbingroup .form-input-hint').html('');
+            $('#parwidthgroup').removeClass('has-error');
+            $('#parwidthgroup .form-input-hint').html('');
+            $('#parcellgroup').removeClass('has-error');
+            $('#parcellgroup .form-input-hint').html('');
+            
+            mainProcess(imWidth, imHeight, cellSize, numBins)
+            
+        });
+    });
+})
+
---
+++ tool_tempo/app.js
@@ -0,0 +1,241 @@
+
+$(document).one('mousedown pointerdown touchstart contextmenu click change', Tone.start);
+
+
+
+$(function(){
+    
+    var vol = new Tone.Volume(-3).toDestination();
+    var synth = new Tone.PolySynth(Tone.Synth).connect(vol);
+    var pitchBias = 0
+    
+    function scrollRelative(e, t){
+        var p = $(e);
+        var c = p.children();
+        c.css('left', (-t * (c.width() - p.width())) + 'px');
+    }
+
+    function changePitch(d){
+        for (var i in keyTriggers){
+            if (keyTriggers[i] != null){
+                keyTriggers[i].clear();
+            }
+        }
+        pitchBias += d;
+        $('#pitchBias').html(pitchBias);
+        updateMark();
+    }
+    
+    function updateMark(){
+        $('.key span').toArray().forEach(item=>item.innerHTML=toFullName(pitchBias+Number(item.parentElement.getAttribute('note'))))
+    }
+    
+    $('#upload').change(function(e) {
+        var file = $('#upload')[0].files[0];
+        if (file == null) return;
+        
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function(e){
+            synth = new Tone.Sampler({
+            	urls: {
+            		"C4": reader.result,
+            	},
+            }).toDestination();
+        }
+    });
+    
+    $('#position').on('input', e=>{
+        scrollRelative($('#keyboard-scroll'), $('#position').val()/100.0);
+    });
+    
+    $('#scale').on('input', e=>{
+        $('#keyboard-scroll').css('font-size', $('#scale').val()+'px');
+        scrollRelative($('#keyboard-scroll'), $('#position').val()/100.0);
+    });
+    
+    $('#hotkeyselect').on('input', e=>{
+        initKeyboard(keysetFromString($('#hotkeyselect').val()));
+    });
+    
+    $('#pitchup').on('click', e=>changePitch(1));
+    $('#pitchdown').on('click', e=>changePitch(-1));
+    
+    var pointerDown = false;
+    $(document).on('pointerdown', e=>pointerDown = true).on('pointerup', e=>pointerDown = false);
+    
+    // name/index conversion
+    var names = {
+        0 : 'C',
+        1 : 'C#',
+        2 : 'D',
+        3 : 'D#',
+        4 : 'E',
+        5 : 'F',
+        6 : 'F#',
+        7 : 'G',
+        8 : 'G#',
+        9 : 'A',
+        10 : 'A#',
+        11 : 'B',
+    };
+    var nums = {
+        'Cb' : -1,
+        'C' : 0,
+        'C#' : 1,
+        'Db' : 1,
+        'D' : 2,
+        'D#' : 3,
+        'Eb' : 3,
+        'E' : 4,
+        'E#' : 5,
+        'Fb' : 4,
+        'F' : 5,
+        'F#' : 6,
+        'Gb' : 6,
+        'G' : 7,
+        'G#' : 8,
+        'Ab' : 8,
+        'A' : 9,
+        'A#' : 10,
+        'Bb' : 10,
+        'B' : 11,
+        'B#' : 12,
+    }
+    
+    var keysetFromString = function(str){
+        var keyset = {};
+        Array.from(str).forEach((c, i)=>{
+            keyset[c] = i;
+        });
+        return keyset;
+    };
+    
+    var toName = function(num){
+        return names[num];
+    }
+    
+    var toNum = function(name){
+        return nums[name];
+    }
+    
+    var toFullNum = function(fullName){
+        return parseInt(fullName.slice(-1)) * 12 + toNum(fullName.slice(0, -1));
+    }
+    
+    var toFullName = function(fullNum){
+        var mod = fullNum % 12;
+        return toName(mod) + ((fullNum - mod) / 12);
+    }
+    
+    var listeners = [];
+    var initKeyboard = function(keyset){
+        listeners.forEach(listener=>{
+            kb.remove(listener);
+        });
+        for (var key in keyset){
+            (function(k, kid){
+                listeners.push(kb.onKeyDown(k, ()=>{
+                    keyTriggers[kid].set('keyboard', true);
+                }));
+                listeners.push(kb.onKeyUp(k, ()=>{
+                    keyTriggers[kid].set('keyboard', false);
+                }));
+            })(key, keyset[key] + 36)
+        }
+    }
+    
+    var keyTriggers = [];
+    var blackDiv = $('#black-keys');
+    var whiteDiv = $('#white-keys');
+    for (var i = 9; i < 97; i++){
+        switch (i % 12){
+            case 1:
+                blackDiv.append(`<div class="key b1" note="${i}"></div>`);
+                break;
+            case 3:
+                blackDiv.append(`<div class="key b2" note="${i}"></div>`);
+                break;
+            case 6:
+                blackDiv.append(`<div class="key b3" note="${i}"></div>`);
+                break;
+            case 8:
+                blackDiv.append(`<div class="key b4" note="${i}"></div>`);
+                break;
+            case 10:
+                blackDiv.append(`<div class="key b5" note="${i}"></div>`);
+                break;
+            case 0:
+                whiteDiv.append(`<div class="key" note="${i}"><span>C${i / 12}</span></div>`);
+                break;
+            default:
+                whiteDiv.append(`<div class="key" note="${i}"></div>`);
+                break;
+        }
+        keyTriggers[i] = {
+            triggers : {},
+            last : false,
+            note : i,
+            set : function(id, stat){
+                this.triggers[id] = stat;
+                var current = this.get();
+                if (this.last && !current){
+                    this.up();
+                }
+                if (current && !this.last){
+                    this.down();
+                }
+                this.last = current;
+            },
+            get : function(){
+                var res = false;
+                for (var id in this.triggers){
+                    res |= this.triggers[id];
+                }
+                return res;
+            },
+            down : function(){
+                synth.triggerAttack(toFullName(parseInt(this.note) + pitchBias));
+                $('.key[note=' + this.note + ']').addClass('pressed');
+            },
+            up : function(){
+                synth.triggerRelease(toFullName(parseInt(this.note) + pitchBias));
+                $('.key[note=' + this.note + ']').removeClass('pressed');
+            },
+            clear : function(){
+                for (var id in this.triggers){
+                    this.set(id, false);
+                }
+            },
+        };
+    }
+    
+    $('.key').on('pointerdown', e=>{
+        keyTriggers[parseInt(e.target.getAttribute('note'))].set('pointer', true);
+    }).on('pointerup pointerleave', e=>{
+        keyTriggers[parseInt(e.target.getAttribute('note'))].set('pointer', false);
+    }).on('pointerenter', e=>{
+        //console.log(pointerDown, e.target.getAttribute('note'));
+        if (pointerDown){
+            keyTriggers[parseInt(e.target.getAttribute('note'))].set('pointer', true);
+        }
+//    }).on('mousedown', e=>{
+//        keyTriggers[parseInt(e.target.getAttribute('note'))].set('mouse', true);
+//    }).on('mouseup mouseleave', e=>{
+//        keyTriggers[parseInt(e.target.getAttribute('note'))].set('mouse', false);
+//    }).on('mouseenter', e=>{
+//        if (pointerDown){
+//            keyTriggers[parseInt(e.target.getAttribute('note'))].set('mouse', true);
+//        }
+    }).on('dragstart selectstart contextmenu', e=>false);
+    
+    
+    
+    //var keyset1 = keysetFromString("AWSEDFTGYHUJKOLP;'");
+    var keyset2 = keysetFromString("ZSXDCVGBHNJM,L.;/Q2W3E4RT6Y7UI9O0P-[]");
+    //var keyset3 = keysetFromString("z.x.cv.b.n.ma.s.df.g.h.jq.w.er.t.y.u1.2.34.5.6.78");
+    
+    //synth.triggerAttackRelease("C4", "8n");
+    initKeyboard(keyset2);
+    scrollRelative($('#keyboard-scroll'), $('#position').val()/100.0);
+});
